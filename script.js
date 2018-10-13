7785240ceed7f757ce89100bd190042ae9b79704
Donna L Campbell <dlcsoup51@me.com>
2018-10-13 14:21:28 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function nameInfo(){
+    var text="firstName";
+   
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(fullName);
+}
+
+function nameInfo(){
+let firstName=document.getElementById().value;
+console.log (firstName);
+let middleName=document.getElementById().value;
+let lastName=document.getElementById().value;
+let nameInfo=document.getElementById(fullName).value;
+
+}
+
+function nameInfo(fullName){
+fullName=document.getElementById(fullName).innerHTML;
+
+var nameIndo = document.getElementById("BUTTON");
+return firstName,middleName,lastName;
+
+ }
